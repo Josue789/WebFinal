7ac791d1c9680617da2b3520824f3fb053c8080d
Josue <mexic@DESKTOP-8QCBTRG>
2023-12-05 23:36:25 -0600

    Cambio apariencia en RegistroEquipo.php

--- Proyecto_Final/vista/RegistroEquipo.php
+++ Proyecto_Final/vista/RegistroEquipo.php
@@ -36,123 +36,126 @@
 
                     <label class="display-3 mt-4 mb-3">Nuevo Equipo</label>
                     <form method="post" class="needs-validation" novalidate>
-                        <p class="d-inline-flex gap-1">
-                        <a class="btn btn-outline-secundary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                            Datos del equipo
-                        </a>
-                        <a class="btn btn-outline-secundary" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
-                            Datos de los Participantes
-                        </a>
-                        </p>
-                        <input type="text" name="Id" value="<?= $equipo->id ?>">
+                        <input type="text" name="Id" value="<?= $equipo->id ?>" hidden>
+                       
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
                         <!--    ++++++++++++++++++++++++++++NOMBRE,CONCURSO,FOTO++++++++++++++++++++++++++++++++++++  -->
-                        <div class="collapse" id="collapseExample">
-                            <div class="card card-body">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                    Datos del equipo
+                                </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                                <div class="accordion-body">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text material-symbols-outlined p-3">
+                                            groups
+                                        </span>
+                                        <div class="form-floating">
+                                            <input name="nombre" type="text" class="form-control <?=$valNombre?>" id="floatingInput" value="<?=$equipo->nombreEquipo?>" required>
+                                            <label for="floatingInput">Nombre</label>
+                                            <div class="invalid-tooltip">
+                                                Campo obligatorio
+                                                <ul>
+                                                    <li>Debe tener por lo menos 3</li>
+                                                    <li>Puede tener maximo 51 caracteres</li>
+                                                    <li>Solo se permiten letras, numeros y los caracteres #,+,.,-</li>
+                                                </ul>
+                                            </div>
+                                        </div>                          
+                                    </div>
 
-                                <h2>Datos del equipo</h2>
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text material-symbols-outlined p-3">
+                                            school
+                                        </span>
+                                        <select name="concurso" class="form-select form-select-lg" aria-label="Large select example" required>
+                                        <?php
+                                                $dao = new daoEquipo();
+                                                $listaConcursos=$dao->obtenerConcursos();
+
+                                                if (ISSET($listaConcursos)) {
+                                                //Carga los renglones en base al registro obtenido
+                                                    foreach ($listaConcursos as $concurso){         
+                                                        echo  "<option value='$concurso->id'>
+                                                                $concurso->nombre
+                                                        </option>";
+                                                    }
+                                                }
+                                            ?>
+                                        </select>
+                                        <div class="invalid-tooltip">
+                                            Campo obligatorio
+                                        </div>
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group mb-3">
+                                            <span class="input-group-text material-symbols-outlined">
+                                                mail
+                                            </span>
+                                            <input name="file" type="file" class="form-control " id="inputGroupFile01">
+                                        </div>
+                                        <div class="invalid-tooltip">
+                                            Campo obligatorio
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+
+                             <!-- +++++++++++++++++++++++++++++++++++++PARTICIPANTES+++++++++++++++++++++++++ -->
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                    Nombres de los integrantes
+                                </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                                <div class="accordion-body">
                                 <div class="input-group mb-3">
-                            <span class="input-group-text material-symbols-outlined p-3">
-                                groups
-                            </span>
-                            <div class="form-floating">
-                                <input name="nombre" type="text" class="form-control <?=$valNombre?>" id="floatingInput" value="<?=$equipo->nombreEquipo?>" required>
-                                <label for="floatingInput">Nombre</label>
-                                <div class="invalid-tooltip">
-                                    Campo obligatorio
-                                    <ul>
-                                        <li>Debe tener por lo menos 3</li>
-                                        <li>Puede tener maximo 51 caracteres</li>
-                                        <li>Solo se permiten letras, numeros y los caracteres #,+,.,-</li>
-                                    </ul>
+                                    <span class="input-group-text material-symbols-outlined p-3">
+                                        person
+                                    </span>
+                                    <div class="form-floating">
+                                        <input name="participante1" type="text" class="form-control <?=$valParticipante1?>" id="floatingInput" value="<?= $equipo->estudiante1 ?>" required>
+                                        <label for="floatingInput">Nombre participante 1</label>
+                                        <div class="invalid-tooltip">
+                                            Campo obligatorio
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>                          
-                        </div>
 
-                        <div class="input-group mb-3">
-                            <span class="input-group-text material-symbols-outlined p-3">
-                                school
-                            </span>
-                            <select name="concurso" class="form-select form-select-lg" aria-label="Large select example" required>
-                            <?php
-                                    $dao = new daoEquipo();
-                                    $listaConcursos=$dao->obtenerConcursos();
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text material-symbols-outlined p-3">
+                                        person
+                                    </span>
+                                    <div class="form-floating">
+                                        <input name="participante2" type="text" class="form-control <?=$valParticipante2?>" id="floatingInput" value="<?=$equipo->estudiante2?>" required>
+                                        <label for="floatingInput">Nombre participante 2</label>
+                                        <div class="invalid-tooltip">
+                                            Campo obligatorio
+                                        </div>
+                                    </div>
+                                </div>
 
-                                    if (ISSET($listaConcursos)) {
-                                    //Carga los renglones en base al registro obtenido
-                                        foreach ($listaConcursos as $concurso){         
-                                            echo  "<option value='$concurso->id'>
-                                                    $concurso->nombre
-                                            </option>";
-                                        }
-                                    }
-                                ?>
-                            </select>
-                              <div class="invalid-tooltip">
-                                Campo obligatorio
-                            </div>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group mb-3">
-                                <span class="input-group-text material-symbols-outlined">
-                                    mail
-                                </span>
-                                <input name="file" type="file" class="form-control " id="inputGroupFile01">
-                            </div>
-                            <div class="invalid-tooltip">
-                                Campo obligatorio
-                            </div>
-                        </div>
-                            </div>
-                        </div>
-                        
-                        <!-- +++++++++++++++++++++++++++++++++++++PARTICIPANTES+++++++++++++++++++++++++ -->
-                        <div class="collapse" id="collapseExample2">
-                            <div class="card card-body">
-                            <h2>Nombre de Participantes</h2>
-                            <div class="input-group mb-3">
-                            <span class="input-group-text material-symbols-outlined p-3">
-                                person
-                            </span>
-                            <div class="form-floating">
-                                <input name="participante1" type="text" class="form-control <?=$valParticipante1?>" id="floatingInput" value="<?= $equipo->estudiante1 ?>" required>
-                                <label for="floatingInput">Nombre participante 1</label>
-                                <div class="invalid-tooltip">
-                                    Campo obligatorio
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text material-symbols-outlined p-3">
+                                        person
+                                    </span>
+                                    <div class="form-floating">
+                                        <input name="participante3" type="text" class="form-control <?=$valParticipante3?>" id="floatingInput" value="<?=$equipo->estudiante3?>" required>
+                                        <label for="floatingInput">Nombre participante 3</label>
+                                        <div class="invalid-tooltip">
+                                            Campo obligatorio
+                                        </div>
+                                    </div>
+                                </div> 
+                                </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <span class="input-group-text material-symbols-outlined p-3">
-                                person
-                            </span>
-                            <div class="form-floating">
-                                <input name="participante2" type="text" class="form-control <?=$valParticipante2?>" id="floatingInput" value="<?=$equipo->estudiante2?>" required>
-                                <label for="floatingInput">Nombre participante 2</label>
-                                <div class="invalid-tooltip">
-                                    Campo obligatorio
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <span class="input-group-text material-symbols-outlined p-3">
-                                person
-                            </span>
-                            <div class="form-floating">
-                                <input name="participante3" type="text" class="form-control <?=$valParticipante3?>" id="floatingInput" value="<?=$equipo->estudiante3?>" required>
-                                <label for="floatingInput">Nombre participante 3</label>
-                                <div class="invalid-tooltip">
-                                    Campo obligatorio
-                                </div>
-                            </div>
-                        </div> 
-                            </div>
-                        </div>
-
-
-                        
+                        </div>                        
                                                                 
                         <div class="row mt-2">
                             <div class="col-6">
